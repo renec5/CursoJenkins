5666fe70db48ab71d3109c78712dbf7d1abbf864
renec5 <42826774+renec5@users.noreply.github.com>
2023-01-13 11:42:25 -0600

    Update nodejsDockerDSL.groovy

--- nodejsDockerDSL.groovy
+++ nodejsDockerDSL.groovy
@@ -16,7 +16,7 @@ job('Aplicacion Node.js Docker DSL') {
         dockerBuildAndPublish {
             repositoryName('renec5/CursoJenkins')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('dockerhub')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
